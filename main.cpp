c33fda15a08183d64ce135ea5c17478c4a1e760d
gomezaldana <aldana0gomez@gmail.com>
2025-05-24 16:56:21 -0300

    creamos archivo main

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include "auxiliares.cpp"
+#include "biblioteca.cpp"
+
+using namespace std;
+
+int main()
+{
+    // preguntar al profe sobre el formato del archivo, si habia una palabra de mas..
+    vector<Biblioteca*> bibliotecas= {};
+    cargarBibliotecas("bibliotecas.txt", bibliotecas);
+
+    cout << "Bibliotecas cargadas: " << endl;
+    for (int i = 0; i < bibliotecas.size(); i++)
+    {
+        bibliotecas[i]->mostrar();
+    } 
+
+    // llibero memoria
+    for (Biblioteca *biblio : bibliotecas)
+    {
+        delete biblio; 
+    }
+}
