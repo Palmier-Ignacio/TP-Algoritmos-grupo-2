2a33ab4ef72ebfaecda1747166a07a349de3966b
gomezaldana <aldana0gomez@gmail.com>
2025-06-06 14:44:52 -0300

    implemento menu

--- gestorBibliotecas.cpp
+++ gestorBibliotecas.cpp
@@ -5,9 +5,8 @@
 #include <string>
 #include "biblioteca.h"
 #include "auxiliares.h"
-#include "tablaHash.h"
+// #include "tablaHash.h"
 #include "arbol.h"
-
 
 using namespace std;
 
@@ -18,11 +17,10 @@ void gestorBibliotecas()
 
     Arbol arbolBibliotecas;
     int cantidadBibliotecas = cargarBibliotecas("bibliotecas.txt", arbolBibliotecas);
-    int tamanioTabla = cantidadBibliotecas/0.8;//fijarnos esto
-    TablaHash tablaBiblioteca =TablaHash(tamanioTabla);
-    // recorrer arbol y cargar tabla
+    int tamanioTabla = cantidadBibliotecas / 0.8; // fijarnos esto
+    // TablaHash tablaBiblioteca = TablaHash(tamanioTabla);
+    //  falta recorrer arbol y cargar tabla
 
-    
     arbolBibliotecas.inorden(arbolBibliotecas.getRaiz());
 
     do
@@ -32,7 +30,7 @@ void gestorBibliotecas()
              << "A) Cargar biblioteca. " << std::endl
              << "B) Buscar biblioteca por codigo.  " << std::endl
              << "C) Eliminar biblioteca existente   " << std::endl
-             << "D) Mostrar todas las bibliotecas (poner las opciones)   " << std::endl
+             << "D) Mostrar todas las bibliotecas  " << std::endl
              << "E) Calcular distancia entre bibliotecas   " << std::endl
              << "F) Calcular el total de prestamos de biblioteca   " << std::endl
              << "G) Detectar bibliotecas con alta carga  " << std::endl
@@ -47,20 +45,58 @@ void gestorBibliotecas()
         case 'a':
         {
             std::cout << "La opción que usted eligió es " << operacion << std::endl;
+            string codigo;
+            string nombre;
+            string ciudad;
+            float superficie;
+            int cantidadLibros;
+            int cantidadUsuarios;
+            cout << "Por favor, ingrese el codigo de la biblioteca que desea cargar: ";
+            cin >> codigo;
+            cout << "Por favor, ingrese el nombre de la biblioteca que desea cargar: ";
+            cin >> nombre;
+            cout << "Por favor, ingrese la ciudad de la biblioteca que desea cargar: ";
+            cin >> ciudad;
+            cout << "Por favor, ingrese la superficie de la biblioteca que desea cargar: ";
+            cin >> superficie;
+            cout << "Por favor, ingrese la cantidad de libros de la biblioteca que desea cargar: ";
+            cin >> cantidadLibros;
+            cout << "Por favor, ingrese la cantidad de usuarios de la biblioteca que desea cargar: ";
+            cin >> cantidadUsuarios;
 
+            Biblioteca *bibliotecaACargar = new Biblioteca(codigo, nombre, ciudad, superficie, cantidadLibros, cantidadUsuarios);
+            arbolBibliotecas.insertar(bibliotecaACargar);
+            // agregar en tabla hash
+            // preguntar si hay q cargarlo en el archivo
             break;
         }
         case 'b':
         {
             std::cout << "La opción que usted eligió es " << operacion << std::endl;
-            
+            string codigoBibliotecaABuscar;
+            cout << "Por favor, ingrese el nombre del producto que desea buscar: ";
+            cin >> codigoBibliotecaABuscar;
+            arbolBibliotecas.buscar(arbolBibliotecas.getRaiz(), codigoBibliotecaABuscar);
+
             break;
         }
         case 'c':
+        {
+            // ERROR AL QUERER BORRAR RAIZ, ARREGLAR
             std::cout << "La opción que usted eligió es " << operacion << std::endl;
-            break;
+            // falta borrar en la tabla
+            string codigoBibliotecaABorrar;
+            cout << "Por favor, ingrese el nombre del producto que desea eliminar: ";
+            cin >> codigoBibliotecaABorrar;
+            arbolBibliotecas.borrar(codigoBibliotecaABorrar);
+        }
+
+        break;
         case 'd':
             std::cout << "La opción que usted eligió es " << operacion << std::endl;
+            //PREGUNTAR SI HAY QUE DAR LAS TRES OPCIONES O SI ELEGIMOS NOSOTROS
+            // DE MOMENTO PONGO EN INORDEN ORDENADAS POR CODIGO
+            arbolBibliotecas.inorden(arbolBibliotecas.getRaiz());
             break;
         case 'e':
             std::cout << "La opción que usted eligió es " << operacion << std::endl;
@@ -73,11 +109,13 @@ void gestorBibliotecas()
             break;
         case 'h':
             std::cout << "Saliendo del menu" << std::endl;
-            
+
             break;
-         case 'i':
+        case 'i':
             std::cout << "Saliendo del menu" << std::endl;
             aux = false;
+            arbolBibliotecas.liberar(arbolBibliotecas.getRaiz());
+
             break;
         default:
             std::cout << "Operación no válida." << std::endl;
@@ -85,5 +123,4 @@ void gestorBibliotecas()
     } while (aux);
 
     arbolBibliotecas.liberar(arbolBibliotecas.getRaiz());
-
 }
